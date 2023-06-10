1794c865c2072061f3fe30ff2f6feb9f24f6dddc
kendall529 <kendall.smith529@gmail.com>
2023-06-09 22:41:11 -0500

    still can't figure out why nothing writes to the page

--- Develop/routes/api/notes.js
+++ Develop/routes/api/notes.js
@@ -1,53 +1,67 @@
 const router = require('express').Router();
 const fs = require('fs');
+const path = require('path');
+const notesPath = path.resolve(__dirname, '../../db/notes.json');
 
 // Function to create unique ids provided in course content
 const uuid = require('../../helpers/uuid');
 
-let db = JSON.parse(fs.readFileSync('./db/notes.json', 'utf8'));
+let data = require('../../db/notes.json');
 
 // GET route for retrieving all notes
 router.get('/', (req, res) => {
   console.info(`${req.method} request received for notes`);
-  res.json(db);
+  fs.readFile('./db/notes.json', 'utf-8', (err, data) => {
+    if(err) throw err;
+    var notes = JSON.parse(data);
+
+    res.json(notes);
+  })
 });
 
 // GET route for retrieving a note by id
 router.get('/:id', (req,res) => {
-  const note = db.find(note => note.note_id === req.params.id);
-  res.json(note);
+    res.json(notes[req.params.id])
 });
 
 // POST Route for a new note
 router.post('/', (req, res) => {
-  console.info(`${req.method} request received to add a note`);
-
-  const { title, text } = req.body;
-
-  if(req.body) {
-    const newNote = {
-      title,
-      text,
-      note_id: uuid(),
-    };
-
-    db.push(newNote);
-
-    fs.writeFileSync('./db/notes.json', JSON.stringify(db));
-
-    res.json(db);
-  } else {
-    res.status(400).send('Error in adding note');
-  }
-});
-
-router.delete('/:id', (req, res) => {
-  db = db.filter((currentNote) => {
-    return currentNote.note_id != req.params.id;
+    console.info(`${req.method} request received to add a note`);
+    
+    fs.readFile('./db/notes.json', 'utf-8', (err, data) => {
+      if(err) throw err;
+      
+      let notes = JSON.parse(data);
+      const { title, text } = req.body;
+    
+      if(req.body) {
+        const newNote = {
+          title,
+          text,
+          note_id: uuid(),
+        };
+  
+        notes.push(newNote);
+        fs.writeFile('./db/notes.json', JSON.stringify(notes, null, 2), (err) => {
+          if(err) throw err;
+          
+          res.json(newNote);
+        });
+      } else {
+        res.status(400).send('Error in adding note');
+      }
+    });
   });
 
-  fs.writeFileSync('./db/notes.json', JSON.stringify(db));
-  res.json(db);
+router.delete('/:id', (req, res) => {
+    const noteToDelete = data.find(note => note.note_id === req.params.id);
+    if(noteToDelete) {
+        data = data.filter(note => note.note_id !== req.params.id);
+        fs.writeFileSync(notesPath, JSON.stringify(data, null, 2));
+        res.json(data);
+    } else {
+        res.status(400).send('Error deleting note');
+    }
 });
 
 module.exports = router;
